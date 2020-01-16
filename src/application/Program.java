74535bf902e54907de8232a45c9b8fab46a29dc1
victhor409 <victhor409@gmail.com>
2020-01-16 16:51:00 -0300

    Covariancia e contravariancia

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,32 +1,32 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import entities.Circle;
-import entities.Rectangle;
-import entities.Shape;
 
 public class Program {
 
 	public static void main(String[] args) {
+
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+		List<Object> myObjs = new ArrayList<Object>();
 		
-		List<Shape> myShapes = new ArrayList<>();
-		myShapes.add(new Rectangle(3.0, 2.0));
-		myShapes.add(new Circle(2.0));
-		
-		List<Circle> myCircles = new ArrayList<>();
-		myCircles.add(new Circle(2.0));
-		myCircles.add(new Circle(3.0));
-		
-		System.out.println("Total area: " + totalArea(myCircles));
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDoubles, myObjs);
+		printList(myObjs);
 	}
-	
-	public static double totalArea(List<? extends Shape> list) {
-		double sum = 0.0;
-		for (Shape s : list) {
-			sum += s.area();
+	                         //Covariancia e contravariancia 
+	public static void copy(List<? extends Number>source,List<? super Number> destiny) {
+		for (Number number : source) {
+			destiny.add(number);
 		}
-		return sum;
+	}
+	public static void printList(List<?>list) {
+		for (Object obj : list) {
+			System.out.println(obj + " ");
+		}
+		System.out.println();
 	}
 }
